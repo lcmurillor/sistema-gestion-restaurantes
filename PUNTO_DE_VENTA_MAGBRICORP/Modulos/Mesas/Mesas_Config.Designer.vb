c028004cc29de6d27ab3f1524c592716889d9490
Luis Carlos Murillo <lcmurillor@hotmail.com>
2025-02-11 18:39:35 -0600

    creacion de las mesas

--- PUNTO_DE_VENTA_MAGBRICORP/Modulos/Mesas/Mesas_Config.Designer.vb
+++ PUNTO_DE_VENTA_MAGBRICORP/Modulos/Mesas/Mesas_Config.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Mesas_Config
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,152 +20,109 @@ Partial Class Mesas_Config
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.PNL_Salones = New System.Windows.Forms.Panel()
         Me.FLP_Salones = New System.Windows.Forms.FlowLayoutPanel()
-        Me.Panel7 = New System.Windows.Forms.Panel()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Panel8 = New System.Windows.Forms.Panel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.PNL_Botones = New System.Windows.Forms.Panel()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.PNL_Busqueda = New System.Windows.Forms.Panel()
         Me.TXT_BuscarSalon = New System.Windows.Forms.TextBox()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.AgregarSalonToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.Panel5 = New System.Windows.Forms.Panel()
-        Me.FlowLayoutPanel2 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.PNL_Mesas = New System.Windows.Forms.Panel()
+        Me.FLP_Mesas = New System.Windows.Forms.FlowLayoutPanel()
         Me.Panel1.SuspendLayout()
-        Me.Panel4.SuspendLayout()
-        Me.FLP_Salones.SuspendLayout()
-        Me.Panel7.SuspendLayout()
-        Me.Panel3.SuspendLayout()
-        Me.Panel2.SuspendLayout()
-        Me.Panel6.SuspendLayout()
+        Me.PNL_Salones.SuspendLayout()
+        Me.PNL_Botones.SuspendLayout()
+        Me.PNL_Busqueda.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
-        Me.Panel5.SuspendLayout()
+        Me.PNL_Mesas.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
-        Me.Panel1.Controls.Add(Me.Panel4)
-        Me.Panel1.Controls.Add(Me.Panel3)
-        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.PNL_Salones)
+        Me.Panel1.Controls.Add(Me.PNL_Botones)
+        Me.Panel1.Controls.Add(Me.PNL_Busqueda)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(349, 450)
         Me.Panel1.TabIndex = 0
         '
-        'Panel4
+        'PNL_Salones
         '
-        Me.Panel4.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.Panel4.Controls.Add(Me.FLP_Salones)
-        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel4.Location = New System.Drawing.Point(0, 100)
-        Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(349, 250)
-        Me.Panel4.TabIndex = 3
+        Me.PNL_Salones.Controls.Add(Me.FLP_Salones)
+        Me.PNL_Salones.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PNL_Salones.Location = New System.Drawing.Point(0, 94)
+        Me.PNL_Salones.Name = "PNL_Salones"
+        Me.PNL_Salones.Size = New System.Drawing.Size(349, 256)
+        Me.PNL_Salones.TabIndex = 3
         '
         'FLP_Salones
         '
         Me.FLP_Salones.AutoScroll = True
-        Me.FLP_Salones.Controls.Add(Me.Panel7)
+        Me.FLP_Salones.AutoSize = True
         Me.FLP_Salones.Dock = System.Windows.Forms.DockStyle.Fill
         Me.FLP_Salones.Location = New System.Drawing.Point(0, 0)
+        Me.FLP_Salones.Margin = New System.Windows.Forms.Padding(0)
         Me.FLP_Salones.Name = "FLP_Salones"
-        Me.FLP_Salones.Size = New System.Drawing.Size(349, 250)
+        Me.FLP_Salones.Size = New System.Drawing.Size(349, 256)
         Me.FLP_Salones.TabIndex = 0
         '
-        'Panel7
+        'PNL_Botones
         '
-        Me.Panel7.Controls.Add(Me.Button5)
-        Me.Panel7.Controls.Add(Me.Panel8)
-        Me.Panel7.Location = New System.Drawing.Point(3, 3)
-        Me.Panel7.Name = "Panel7"
-        Me.Panel7.Size = New System.Drawing.Size(346, 80)
-        Me.Panel7.TabIndex = 0
-        '
-        'Button5
-        '
-        Me.Button5.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.Button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Button5.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button5.Location = New System.Drawing.Point(10, 0)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(336, 80)
-        Me.Button5.TabIndex = 1
-        Me.Button5.Text = "Button5"
-        Me.Button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.Button5.UseVisualStyleBackColor = False
-        '
-        'Panel8
-        '
-        Me.Panel8.BackColor = System.Drawing.Color.Coral
-        Me.Panel8.Dock = System.Windows.Forms.DockStyle.Left
-        Me.Panel8.ForeColor = System.Drawing.SystemColors.ActiveBorder
-        Me.Panel8.Location = New System.Drawing.Point(0, 0)
-        Me.Panel8.Name = "Panel8"
-        Me.Panel8.Size = New System.Drawing.Size(10, 80)
-        Me.Panel8.TabIndex = 0
-        '
-        'Panel3
-        '
-        Me.Panel3.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.Panel3.Controls.Add(Me.Button3)
-        Me.Panel3.Controls.Add(Me.Button4)
-        Me.Panel3.Controls.Add(Me.Button2)
-        Me.Panel3.Controls.Add(Me.Button1)
-        Me.Panel3.Controls.Add(Me.Label2)
-        Me.Panel3.Controls.Add(Me.Label1)
-        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel3.Location = New System.Drawing.Point(0, 350)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(349, 100)
-        Me.Panel3.TabIndex = 2
+        Me.PNL_Botones.Controls.Add(Me.Button3)
+        Me.PNL_Botones.Controls.Add(Me.Button4)
+        Me.PNL_Botones.Controls.Add(Me.Button2)
+        Me.PNL_Botones.Controls.Add(Me.Button1)
+        Me.PNL_Botones.Controls.Add(Me.Label2)
+        Me.PNL_Botones.Controls.Add(Me.Label1)
+        Me.PNL_Botones.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.PNL_Botones.Location = New System.Drawing.Point(0, 350)
+        Me.PNL_Botones.Name = "PNL_Botones"
+        Me.PNL_Botones.Size = New System.Drawing.Size(349, 100)
+        Me.PNL_Botones.TabIndex = 2
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(241, 49)
+        Me.Button3.Location = New System.Drawing.Point(308, 53)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.Size = New System.Drawing.Size(23, 23)
         Me.Button3.TabIndex = 5
         Me.Button3.Text = "+"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(160, 49)
+        Me.Button4.Location = New System.Drawing.Point(279, 53)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.Size = New System.Drawing.Size(23, 23)
         Me.Button4.TabIndex = 4
         Me.Button4.Text = "-"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(241, 19)
+        Me.Button2.Location = New System.Drawing.Point(308, 23)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(23, 23)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "+"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(160, 19)
+        Me.Button1.Location = New System.Drawing.Point(279, 23)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(23, 23)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "-"
         Me.Button1.UseVisualStyleBackColor = True
@@ -173,7 +130,7 @@ Partial Class Mesas_Config
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(55, 54)
+        Me.Label2.Location = New System.Drawing.Point(174, 58)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(99, 13)
         Me.Label2.TabIndex = 1
@@ -182,40 +139,31 @@ Partial Class Mesas_Config
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(60, 24)
+        Me.Label1.Location = New System.Drawing.Point(179, 28)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(94, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Tamaño del Botón"
         '
-        'Panel2
+        'PNL_Busqueda
         '
-        Me.Panel2.BackColor = System.Drawing.SystemColors.ControlDark
-        Me.Panel2.Controls.Add(Me.Panel6)
-        Me.Panel2.Controls.Add(Me.MenuStrip1)
-        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel2.Location = New System.Drawing.Point(0, 0)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(349, 100)
-        Me.Panel2.TabIndex = 1
-        '
-        'Panel6
-        '
-        Me.Panel6.Controls.Add(Me.TXT_BuscarSalon)
-        Me.Panel6.Location = New System.Drawing.Point(12, 39)
-        Me.Panel6.Name = "Panel6"
-        Me.Panel6.Size = New System.Drawing.Size(200, 39)
-        Me.Panel6.TabIndex = 1
+        Me.PNL_Busqueda.Controls.Add(Me.TXT_BuscarSalon)
+        Me.PNL_Busqueda.Controls.Add(Me.MenuStrip1)
+        Me.PNL_Busqueda.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PNL_Busqueda.Location = New System.Drawing.Point(0, 0)
+        Me.PNL_Busqueda.Name = "PNL_Busqueda"
+        Me.PNL_Busqueda.Size = New System.Drawing.Size(349, 94)
+        Me.PNL_Busqueda.TabIndex = 1
         '
         'TXT_BuscarSalon
         '
         Me.TXT_BuscarSalon.AccessibleDescription = ""
         Me.TXT_BuscarSalon.AccessibleName = ""
-        Me.TXT_BuscarSalon.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.TXT_BuscarSalon.Location = New System.Drawing.Point(0, 0)
+        Me.TXT_BuscarSalon.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TXT_BuscarSalon.Location = New System.Drawing.Point(12, 43)
         Me.TXT_BuscarSalon.Name = "TXT_BuscarSalon"
         Me.TXT_BuscarSalon.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.TXT_BuscarSalon.Size = New System.Drawing.Size(200, 20)
+        Me.TXT_BuscarSalon.Size = New System.Drawing.Size(200, 29)
         Me.TXT_BuscarSalon.TabIndex = 0
         Me.TXT_BuscarSalon.Text = "Buscar..."
         '
@@ -234,69 +182,62 @@ Partial Class Mesas_Config
         Me.AgregarSalonToolStripMenuItem.Size = New System.Drawing.Size(104, 20)
         Me.AgregarSalonToolStripMenuItem.Text = "+ Agregar Salon"
         '
-        'Panel5
+        'PNL_Mesas
         '
-        Me.Panel5.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.Panel5.Controls.Add(Me.FlowLayoutPanel2)
-        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.Panel5.Location = New System.Drawing.Point(349, 0)
-        Me.Panel5.Name = "Panel5"
-        Me.Panel5.Size = New System.Drawing.Size(451, 450)
-        Me.Panel5.TabIndex = 1
+        Me.PNL_Mesas.Controls.Add(Me.FLP_Mesas)
+        Me.PNL_Mesas.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PNL_Mesas.Location = New System.Drawing.Point(349, 0)
+        Me.PNL_Mesas.Name = "PNL_Mesas"
+        Me.PNL_Mesas.Size = New System.Drawing.Size(451, 450)
+        Me.PNL_Mesas.TabIndex = 1
         '
-        'FlowLayoutPanel2
+        'FLP_Mesas
         '
-        Me.FlowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.FlowLayoutPanel2.Location = New System.Drawing.Point(0, 0)
-        Me.FlowLayoutPanel2.Name = "FlowLayoutPanel2"
-        Me.FlowLayoutPanel2.Size = New System.Drawing.Size(451, 450)
-        Me.FlowLayoutPanel2.TabIndex = 0
+        Me.FLP_Mesas.BackColor = System.Drawing.SystemColors.Control
+        Me.FLP_Mesas.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FLP_Mesas.Location = New System.Drawing.Point(0, 0)
+        Me.FLP_Mesas.Name = "FLP_Mesas"
+        Me.FLP_Mesas.Size = New System.Drawing.Size(451, 450)
+        Me.FLP_Mesas.TabIndex = 0
         '
         'Mesas_Config
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.PNL_Mesas)
         Me.Controls.Add(Me.Panel1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Mesas_Config"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Panel1.ResumeLayout(False)
-        Me.Panel4.ResumeLayout(False)
-        Me.FLP_Salones.ResumeLayout(False)
-        Me.Panel7.ResumeLayout(False)
-        Me.Panel3.ResumeLayout(False)
-        Me.Panel3.PerformLayout()
-        Me.Panel2.ResumeLayout(False)
-        Me.Panel2.PerformLayout()
-        Me.Panel6.ResumeLayout(False)
-        Me.Panel6.PerformLayout()
+        Me.PNL_Salones.ResumeLayout(False)
+        Me.PNL_Salones.PerformLayout()
+        Me.PNL_Botones.ResumeLayout(False)
+        Me.PNL_Botones.PerformLayout()
+        Me.PNL_Busqueda.ResumeLayout(False)
+        Me.PNL_Busqueda.PerformLayout()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
-        Me.Panel5.ResumeLayout(False)
+        Me.PNL_Mesas.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents Panel4 As Panel
-    Friend WithEvents Panel3 As Panel
-    Friend WithEvents Panel2 As Panel
-    Friend WithEvents Panel5 As Panel
-    Friend WithEvents Panel6 As Panel
+    Friend WithEvents PNL_Botones As Panel
+    Friend WithEvents PNL_Busqueda As Panel
+    Friend WithEvents PNL_Mesas As Panel
     Friend WithEvents MenuStrip1 As MenuStrip
     Friend WithEvents AgregarSalonToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents TXT_BuscarSalon As TextBox
     Friend WithEvents FLP_Salones As FlowLayoutPanel
-    Friend WithEvents FlowLayoutPanel2 As FlowLayoutPanel
+    Friend WithEvents FLP_Mesas As FlowLayoutPanel
     Friend WithEvents Label1 As Label
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button1 As Button
     Friend WithEvents Label2 As Label
-    Friend WithEvents Panel7 As Panel
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Panel8 As Panel
+    Friend WithEvents PNL_Salones As Panel
 End Class
--- PUNTO_DE_VENTA_MAGBRICORP/Modulos/Mesas/Mesas_Config.vb
+++ PUNTO_DE_VENTA_MAGBRICORP/Modulos/Mesas/Mesas_Config.vb
@@ -1,10 +1,9 @@
 ﻿Imports System.Data.SqlClient
 
-
 Public Class Mesas_Config
 
-    Dim salonSelecioando As String
-
+    Dim salonSeleccionado As String = ""
+    Dim id_Salon As Integer = 0
 
     Sub dibujarSalones()
         FLP_Salones.Controls.Clear()
@@ -25,35 +24,30 @@ Public Class Mesas_Config
         End Try
     End Sub
 
-    Sub dibujarSalon(reader)
+    Sub dibujarSalon(reader As SqlDataReader)
         Dim boton As New Button()
         Dim panelPrincipal As New Panel()
         Dim panelLateral As New Panel()
 
-        boton.Text = reader("Nombre").ToString()
-        boton.Name = reader("Id_Salon").ToString()
+        boton.Text = If(Not IsDBNull(reader("Nombre")), reader("Nombre").ToString(), "Sin Nombre")
+        boton.Name = If(Not IsDBNull(reader("Id_Salon")), reader("Id_Salon").ToString(), "0")
         boton.Dock = DockStyle.Fill
         boton.BackColor = Color.Transparent
-        boton.Font = New System.Drawing.Font(10, 12)
+        boton.Font = New System.Drawing.Font("Arial", 12)
         boton.FlatStyle = FlatStyle.Flat
         boton.FlatAppearance.BorderSize = 0
-        boton.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64)
-        boton.FlatAppearance.MouseOverBackColor = Color.FromArgb(43, 43, 43)
-        boton.ForeColor = Color.White
+        'boton.FlatAppearance.MouseDownBackColor = Color.FromArgb(64, 64, 64)
+        'boton.FlatAppearance.MouseOverBackColor = Color.FromArgb(43, 43, 43)
         boton.TextAlign = ContentAlignment.MiddleLeft
 
-        panelPrincipal.Size = New System.Drawing.Size(349, 80)
 
-        panelLateral.Size = New System.Drawing.Size(5, 80)
+
+        panelPrincipal.Size = New System.Drawing.Size(FLP_Salones.ClientSize.Width * 0.9, FLP_Salones.ClientSize.Height * 0.15)
+        panelPrincipal.Tag = boton.Name ' Guardamos el ID del salón en el panel
+
+        ' Configuración del panel lateral
+        panelLateral.Size = New System.Drawing.Size(panelPrincipal.ClientSize.Width * 0.01, FLP_Salones.ClientSize.Height)
         panelLateral.Dock = DockStyle.Left
-
-        If salonSelecioando = boton.Text Then
-            panelLateral.BackColor = Color.OrangeRed
-            boton.BackColor = Color.FromArgb(43, 43, 43)
-        Else
-            panelLateral.BackColor = Color.Transparent
-            boton.BackColor = Color.Transparent
-        End If
 
         panelPrincipal.Controls.Add(boton)
         panelPrincipal.Controls.Add(panelLateral)
@@ -62,22 +56,102 @@ Public Class Mesas_Config
         boton.BringToFront()
         panelLateral.SendToBack()
 
-        AddHandler boton.Click, AddressOf btnSalonEnvento
-
+        AddHandler boton.Click, AddressOf btnSalonEvento
     End Sub
 
-    Dim id_Salon As Integer
-    Private Sub btnSalonEnvento(ByVal sender As System.Object, ByVal e As EventArgs)
+    ' Evento al hacer clic en un botón de salón
+    Private Sub btnSalonEvento(ByVal sender As Object, ByVal e As EventArgs)
         Try
-            salonSelecioando = DirectCast(sender, Button).Text
-            id_Salon = DirectCast(sender, Button).Name
-            dibujarSalones()
+            Dim botonSeleccionado As Button = DirectCast(sender, Button)
+            salonSeleccionado = botonSeleccionado.Text
+            id_Salon = Convert.ToInt32(botonSeleccionado.Name)
+
+            ' Redibujar solo los paneles para actualizar la selección sin borrar todo
+            For Each panel As Panel In FLP_Salones.Controls
+                Dim panelLateral As Control = panel.Controls(1)
+                Dim boton As Button = panel.Controls(0)
+
+                If boton.Text = salonSeleccionado Then
+                    panelLateral.BackColor = Color.OrangeRed
+                    boton.BackColor = Color.FromArgb(43, 43, 43)
+                    boton.ForeColor = Color.White
+                Else
+                    panelLateral.BackColor = Color.Transparent
+                    boton.BackColor = Color.Transparent
+                    boton.ForeColor = Color.Black
+                End If
+
+                boton.BringToFront()
+                panelLateral.SendToBack()
+            Next
+
+            ' Ahora sí, dibujar las mesas del salón seleccionado
+            dibujarMesas()
+
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
     End Sub
 
+
+    ' Evento cuando el usuario escribe en el cuadro de búsqueda
     Private Sub TXT_BuscarSalon_TextChanged(sender As Object, e As EventArgs) Handles TXT_BuscarSalon.TextChanged
         dibujarSalones()
     End Sub
+
+    ' Método para dibujar las mesas en el FlowLayoutPanel
+    Sub dibujarMesas()
+        FLP_Mesas.Controls.Clear()
+        Try
+            abrir()
+            Dim query As String = "mostrarMesasPorSalon"
+            Dim cmd As New SqlCommand(query, conexion)
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Parameters.AddWithValue("@Id_Salon", id_Salon)
+
+            Dim reader As SqlDataReader = cmd.ExecuteReader()
+            While reader.Read()
+                dibujarMesa(reader)
+            End While
+            reader.Close()
+            Cerrar()
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    ' Método para dibujar una mesa
+    Sub dibujarMesa(reader As SqlDataReader)
+        Dim boton As New Button()
+        ' Manejo de DBNull para evitar errores
+        Dim alto As Integer = If(Not IsDBNull(reader("Y")), Convert.ToInt32(reader("Y")), 50)
+        Dim ancho As Integer = If(Not IsDBNull(reader("X")), Convert.ToInt32(reader("X")), 50)
+        Dim estado As Boolean = If(Not IsDBNull(reader("Estado")), reader("Estado"), False)
+        Dim tamanioLetra As Integer = If(Not IsDBNull(reader("Tamanio_Letra")), Convert.ToInt32(reader("Tamanio_Letra")), 12)
+
+        Dim tamanio As Size = New Size(ancho, alto)
+
+        ' Configuración del botón
+        boton.Text = If(Not IsDBNull(reader("Nombre")), reader("Nombre").ToString(), "Sin Nombre")
+        boton.Name = If(Not IsDBNull(reader("Id_Salon")), reader("Id_Salon").ToString(), "0")
+        boton.Size = tamanio
+
+        boton.Font = New System.Drawing.Font("Arial", tamanioLetra)
+        boton.FlatStyle = FlatStyle.Flat
+        boton.FlatAppearance.BorderSize = 0
+        boton.ForeColor = Color.White
+        boton.Cursor = Cursors.Hand
+        boton.Tag = If(Not IsDBNull(reader("Id_Mesa")), reader("Id_Mesa").ToString(), "0")
+
+        ' Agregar botón o panel al FlowLayoutPanel
+        If estado Then
+            boton.BackColor = Color.Green
+
+        Else
+            boton.BackColor = Color.DarkRed
+        End If
+
+        FLP_Mesas.Controls.Add(boton)
+    End Sub
+
 End Class
